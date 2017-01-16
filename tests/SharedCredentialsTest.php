d1d7dda6a2a35a3fc9824be9d6986b2917fad61a
Oshion Niemela <oshion@smartystreets.com>
2017-01-16 15:46:42 -0700

    Created SharedCredentials class. Got it under test.

---
+++ tests/SharedCredentialsTest.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once(dirname(dirname(__FILE__)) . '/src/smartystreets/api/Request.php');
+require_once(dirname(dirname(__FILE__)) . '/src/smartystreets/api/SharedCredentials.php');
+use smartystreets\api\Request as Request;
+use smartystreets\api\SharedCredentials as SharedCredentials;
+
+class SharedCredentialsTest extends PHPUnit_Framework_TestCase {
+    public function testSignedRequest() {
+        $request = $this->createSignedRequest();
+        $expected = "https://us-street.api.smartystreets.com/street-address?auth-id=3516378604772256";
+
+        $this->assertEquals($expected, $request->getUrl());
+    }
+
+    public function testReferringHeader() {
+        $request = $this->createSignedRequest();
+
+        $this->assertEquals("https://example.com", $request->getHeaders()["Referer"]);
+    }
+
+    private function createSignedRequest() {
+        $mobile = new SharedCredentials("3516378604772256", "example.com");
+        $request = new Request("https://us-street.api.smartystreets.com/street-address?");
+        $mobile->sign($request);
+        return $request;
+    }
+
+}
