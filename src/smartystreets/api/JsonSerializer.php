a23d05d8391c53ae5d51696225b0a5ad8c294b13
Oshion Niemela <oshion@smartystreets.com>
2017-01-23 11:10:59 -0700

    Stubed for JsonSerializer

---
+++ src/smartystreets/api/JsonSerializer.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace smartystreets;
+
+use smartystreets\api\Serializer;
+
+include_once('Serializer.php');
+
+class JsonSerializer implements Serializer {
+    function serialize($obj, $classType) {
+        // TODO: Implement serialize() method.
+    }
+
+    function deserialize($payload, $classType) {
+        // TODO: Implement deserialize() method.
+    }
+
+
+}
