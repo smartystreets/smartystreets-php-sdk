3b5d96f1c47cf530abc641dbe3a33a45291dd120
Neo Pimentel <neo@smartystreets.com>
2017-01-26 16:00:17 -0700

    Implemented us_street’s Batch class.

--- src/smartystreets/api/us_street/Batch.php
+++ src/smartystreets/api/us_street/Batch.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace smartystreets\api\us_zipcode;
+namespace smartystreets\api\us_street;
 
 class Batch {
 
     const MAX_BATCH_SIZE = 100;
     private $namedLookups,
-            $allLookups;
+        $allLookups;
 
     public function __construct() {
         $this->namedLookups = array();
