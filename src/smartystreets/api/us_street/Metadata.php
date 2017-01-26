f967aa8db04fe3c9bc618d3fdbdf9fc8f9422708
Neo Pimentel <neo@smartystreets.com>
2017-01-26 10:56:07 -0700

    Implemented Metadata class.

--- src/smartystreets/api/us_street/Metadata.php
+++ src/smartystreets/api/us_street/Metadata.php
@@ -5,6 +5,39 @@ namespace smartystreets\api\us_street;
 
 
 class Metadata {
+    private $recordType,
+            $zipType,
+            $countyFips,
+            $countyName,
+            $carrierRoute,
+            $congressionalDistrict,
+            $buildingDefaultIndicator,
+            $rdi,
+            $elotSequence,
+            $elotSort,
+            $latitude,
+            $longitude,
+            $precision,
+            $timeZone,
+            $utcOffset,
+            $obeysDst;
+
     public function __construct($obj) {
+        $this->recordType = $obj['record_type'];
+        $this->zipType = $obj['zip_type'];
+        $this->countyFips = $obj['county_fips'];
+        $this->countyName = $obj['county_name'];
+        $this->carrierRoute = $obj['carrier_route'];
+        $this->congressionalDistrict = $obj['congressional_district'];
+        $this->buildingDefaultIndicator = $obj['building_default_indicator'];
+        $this->rdi = $obj['rdi'];
+        $this->elotSequence = $obj['elot_sequence'];
+        $this->elotSort = $obj['elot_sort'];
+        $this->latitude = $obj['latitude'];
+        $this->longitude = $obj['longitude'];
+        $this->precision = $obj['precision'];
+        $this->timeZone = $obj['time_zone'];
+        $this->utcOffset = $obj['utc_offset'];
+        $this->obeysDst = $obj['dst'];
     }
 }
