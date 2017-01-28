349af744618a88d564ed5404adfc0c586da37cd5
Oshion Niemela <oshion@smartystreets.com>
2017-01-27 17:05:58 -0700

    Created a single lookup example.

--- src/smartystreets/examples/UsZipCodeSingleLookupExample.php
+++ src/smartystreets/examples/UsZipCodeSingleLookupExample.php
@@ -2,8 +2,53 @@
 
 namespace smartystreets\examples;
 
+require_once(dirname(dirname(__FILE__)) . '/api/us_zipcode/ClientBuilder.php');
+//require_once(dirname(dirname(__FILE__)) . '/api/us_zipcode/Client.php');
+require_once(dirname(dirname(__FILE__)) . '/api/StaticCredentials.php');
+use smartystreets\api\exceptions\SmartyException;
+use smartystreets\api\StaticCredentials;
+use smartystreets\api\us_zipcode\Lookup;
+use smartystreets\api\us_zipcode\ClientBuilder;
+use smartystreets\api\us_zipcode\Client;
+
+$lookupExample = new UsZipCodeSingleLookupExample();
+$lookupExample->run();
 
 class UsZipCodeSingleLookupExample {
-//    public
 
+    public function run() {
+//        $staticCredentials = new StaticCredentials($_ENV['SMARTY_AUTH_ID'], $_ENV['SMARTY_AUTH_TOKEN']);
+        $staticCredentials = new StaticCredentials('3fa4c814-7015-5345-cc65-3e169e095b04', '6qGUjYXqayyGVYn56OmV');
+        $client = (new ClientBuilder($staticCredentials))->build();
+
+        $lookup = new Lookup();
+        $lookup->setCity("Mountain View");
+        $lookup->setState("California");
+
+        try {
+            $client->sendLookup($lookup);
+        }
+        catch (SmartyException $ex) {
+            echo($ex->getMessage());
+        }
+        catch (\Exception $ex) {
+            echo($ex->getMessage());
+        }
+
+        $result = $lookup->getResult();
+        $zipCodes = $result->getZipCodes();
+        $cities = $result->getCities();
+
+        foreach ($cities as $city) {
+            echo("\nCity: " . $city->getCity());
+            echo("State: " . $city->getState());
+            echo("Mailable City: " . $city->getMailableCity());
+        }
+
+        foreach ($zipCodes as $zip) {
+            echo("\nZIP Code: " . $zip->getZipCode());
+            echo("Latitude: " . $zip->getLatitude());
+            echo("Longitude: " . $zip->getLongitude());
+        }
+    }
 }
