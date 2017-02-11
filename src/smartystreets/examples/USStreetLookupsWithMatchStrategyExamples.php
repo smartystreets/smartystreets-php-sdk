b766dd80e39638c9ac0b4465ac2c3ba4efa670e9
Oshion Niemela <oshion@smartystreets.com>
2017-02-11 02:55:31 -0700

    Created the USStreetLookupsWithMatchStrategyExamples class.

--- src/smartystreets/examples/USStreetLookupsWithMatchStrategyExamples.php
+++ src/smartystreets/examples/USStreetLookupsWithMatchStrategyExamples.php
@@ -12,34 +12,38 @@ use smartystreets\api\ClientBuilder;
 use smartystreets\api\us_street\Lookup;
 use smartystreets\api\Batch;
 
-$lookupExample = new UsStreetMultipleAddressesExample();
+$lookupExample = new USStreetLookupsWithMatchStrategyExamples();
 $lookupExample->run();
 
-class UsStreetMultipleAddressesExample {
+class USStreetLookupsWithMatchStrategyExamples {
+
     public function run() {
         $staticCredentials = new StaticCredentials(getenv('SMARTY_AUTH_ID'), getenv('SMARTY_AUTH_TOKEN'));
         $client = (new ClientBuilder($staticCredentials))->buildStreetClient();
         $batch = new Batch();
 
-        $address0 = new Lookup();
-        $address0->setStreet("1600 amphitheatre parkway");
-        $address0->setCity("Mountain view");
-        $address0->setState("california");
+        $addressWithStrictStrategy = new Lookup();
+        $addressWithStrictStrategy->setStreet("691 W 1150 S");
+        $addressWithStrictStrategy->setCity("provo");
+        $addressWithStrictStrategy->setState("utah");
+        $addressWithStrictStrategy->setMatchStrategy(Lookup::STRICT);
 
-        $address1 = new Lookup("1 Rosedale, Baltimore, Maryland"); // Freeform addresses work too.
-        $address1->setMaxCandidates(10); // Allows up to ten possible matches to be returned (default is 1).
+        $addressWithRangeStrategy = new Lookup();
+        $addressWithRangeStrategy->setStreet("693 W 1150 S");
+        $addressWithRangeStrategy->setCity("provo");
+        $addressWithRangeStrategy->setState("utah");
+        $addressWithRangeStrategy->setMatchStrategy(Lookup::RANGE);
 
-        $address2 = new Lookup("123 Bogus Street, Pretend Lake, Oklahoma");
-
-        $address3 = new Lookup();
-        $address3->setStreet("1 Infinite Loop");
-        $address3->setZipCode("95014"); // You can just input the street and ZIP if you want.
+        $addressWithInvalidStrategy = new Lookup();
+        $addressWithInvalidStrategy->setStreet("9999 W 1150 S");
+        $addressWithInvalidStrategy->setCity("provo");
+        $addressWithInvalidStrategy->setState("utah");
+        $addressWithInvalidStrategy->setMatchStrategy(Lookup::INVALID);
 
         try {
-            $batch->add($address0);
-            $batch->add($address1);
-            $batch->add($address2);
-            $batch->add($address3);
+            $batch->add($addressWithStrictStrategy);
+            $batch->add($addressWithRangeStrategy);
+            $batch->add($addressWithInvalidStrategy);
 
             $client->sendBatch($batch);
             $this->displayResults($batch);
@@ -58,7 +62,7 @@ class UsStreetMultipleAddressesExample {
     public function displayResults(Batch $batch) {
         $lookups = $batch->getAllLookups();
 
-        for ($i = 0; $i < $batch->size(); $i++) {
+        for($i = 0; $i < $batch->size(); $i++) {
             $candidates = $lookups[$i]->getResult();
 
             if (empty($candidates)) {
@@ -72,7 +76,8 @@ class UsStreetMultipleAddressesExample {
                 $components = $candidate->getComponents();
                 $metadata = $candidate->getMetadata();
 
-                echo("\n\nCandidate " . $candidate->getCandidateIndex() . ":");
+                echo("\n\nCandidate " . $candidate->getCandidateIndex() . " ");
+                echo("with " . $lookups[$i]->getMatchStrategy() . " strategy");
                 echo("\nDelivery line 1: " . $candidate->getDeliveryLine1());
                 echo("\nLast line:       " . $candidate->getLastLine());
                 echo("\nZIP Code:        " . $components->getZipCode() . "-" . $components->getPlus4Code());
