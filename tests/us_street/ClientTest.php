4b04df3366d7e75766cf78a640d6a50264aefad8
Oshion Niemela <oshion@smartystreets.com>
2017-04-03 21:43:13 -0600

    Added single lookup tests to US_Street Client

--- tests/us_street/ClientTest.php
+++ tests/us_street/ClientTest.php
@@ -11,6 +11,9 @@ require_once(dirname(dirname(dirname(__FILE__))) . '/src/US_Street/Lookup.php');
 require_once(dirname(dirname(dirname(__FILE__))) . '/src/US_Street/Candidate.php');
 require_once(dirname(dirname(dirname(__FILE__))) . '/src/Batch.php');
 require_once(dirname(dirname(dirname(__FILE__))) . '/src/Response.php');
+require_once(dirname(dirname(dirname(__FILE__))) . '/src/URLPrefixSender.php');
+require_once(dirname(dirname(dirname(__FILE__))) . '/src/NativeSerializer.php');
+use SmartyStreets\PhpSdk\NativeSerializer;
 use SmartyStreets\PhpSdk\Tests\Mocks\MockSerializer;
 use SmartyStreets\PhpSdk\Tests\Mocks\MockDeserializer;
 use SmartyStreets\PhpSdk\Tests\Mocks\RequestCapturingSender;
@@ -20,8 +23,52 @@ use SmartyStreets\PhpSdk\US_Street\Lookup;
 use SmartyStreets\PhpSdk\US_Street\Candidate;
 use SmartyStreets\PhpSdk\Batch;
 use SmartyStreets\PhpSdk\Response;
+use SmartyStreets\PhpSdk\URLPrefixSender;
 
 class ClientTest extends \PHPUnit_Framework_TestCase {
+
+    //region [ Single Lookup ]
+
+    public function testSendingSingleFreeformLookup() {
+        $expectedPayload = "Hello, World!";
+        $sender = new RequestCapturingSender();
+        $serializer = new MockSerializer($expectedPayload);
+        $client = new Client($sender, $serializer);
+
+        $client->sendLookup(new Lookup("freeform"));
+
+        $this->assertEquals($expectedPayload, $sender->getRequest()->getPayload());
+    }
+
+    public function testSendingSingleFullyPopulatedLookup() {
+        $capturingSender = new RequestCapturingSender();
+        $sender = new URLPrefixSender("http://localhost/", $capturingSender);
+        $serializer = new NativeSerializer();
+        $expectedPayload = ("[{\"input_id\":1,\"street\":\"2\",\"street2\":\"3\",\"secondary\":\"4\",\"city\":\"5\"," .
+            "\"state\":\"6\",\"zipcode\":\"7\",\"lastline\":\"8\",\"addressee\":\"9\"," .
+            "\"urbanization\":\"10\",\"match\":\"invalid\",\"candidates\":12}]");
+
+        $client = new Client($sender, $serializer);
+        $lookup = new Lookup();
+        $lookup->setInputId(1);
+        $lookup->setStreet("2");
+        $lookup->setStreet2("3");
+        $lookup->setSecondary("4");
+        $lookup->setCity("5");
+        $lookup->setState("6");
+        $lookup->setZipCode("7");
+        $lookup->setLastline("8");
+        $lookup->setAddressee("9");
+        $lookup->setUrbanization("10");
+        $lookup->setMatchStrategy(Lookup::INVALID);
+        $lookup->setMaxCandidates(12);
+
+        $client->sendLookup($lookup);
+
+        $this->assertEquals($expectedPayload, $capturingSender->getRequest()->getPayload());
+}
+
+//endregion
 
     //region [Batch Lookup ]
 
