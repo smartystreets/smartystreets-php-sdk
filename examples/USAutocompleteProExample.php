dafeb67bac28f53e0cbe3b0fae328759838f7630
Duncan Beutler <duncan@smartystreets.com>
2021-12-14 15:43:03 -0700

    Code to access static credentials in US Autocomplete Pro provided in example.

--- examples/USAutocompleteProExample.php
+++ examples/USAutocompleteProExample.php
@@ -1,11 +1,13 @@
 <?php
 
 require_once(dirname(dirname(__FILE__)) . '/src/SharedCredentials.php');
+require_once(dirname(dirname(__FILE__)) . '/src/StaticCredentials.php');
 require_once(dirname(dirname(__FILE__)) . '/src/ClientBuilder.php');
 require_once(dirname(dirname(__FILE__)) . '/src/US_Autocomplete_Pro/Lookup.php');
 require_once(dirname(dirname(__FILE__)) . '/src/US_Autocomplete_Pro/Client.php');
 
 use SmartyStreets\PhpSdk\SharedCredentials;
+use SmartyStreets\PhpSdk\StaticCredentials;
 use SmartyStreets\PhpSdk\ClientBuilder;
 use SmartyStreets\PhpSdk\US_Autocomplete_Pro\Lookup;
 use SmartyStreets\PhpSdk\US_Autocomplete_Pro\Suggestion;
@@ -17,19 +19,21 @@ class USAutocompleteProExample
 {
     public function run()
     {
-        $key = 'Your SmartyStreets website key here';
-        $hostname = 'Your SmartyStreets website domain here';
-
-        // We recommend storing your secret keys in environment variables instead---it's safer!
+        // We recommend storing your secret keys in environment variables---it's safer!
 //        $key = getenv('SMARTY_WEBSITE_KEY');
 //        $hostname = getenv('SMARTY_WEBSITE_DOMAIN');
 
-        $sharedCredentials = new SharedCredentials($key, $hostname);
+//        $credentials = new SharedCredentials($key, $hostname);
+
+        $id = getenv('SMARTY_AUTH_ID');
+        $token = getenv('SMARTY_AUTH_TOKEN');
+
+        $credentials = new StaticCredentials($id, $token);
 
         // The appropriate license values to be used for your subscriptions
         // can be found on the Subscriptions page the account dashboard.
         // https://www.smartystreets.com/docs/cloud/licensing
-        $client = (new ClientBuilder($sharedCredentials)) ->withLicenses(["us-autocomplete-pro-cloud"])
+        $client = (new ClientBuilder($credentials)) ->withLicenses(["us-autocomplete-pro-cloud"])
             ->buildUSAutocompleteProApiClient();
 
         // Documentation for input fields can be found at:
