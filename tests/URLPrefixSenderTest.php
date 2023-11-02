ade5143e133b7cb6b5b999fb50b5657687783b79
Landon Fackrell <landon@smarty.com>
2023-11-02 11:55:32 -0600

    Fix URL prefix sender test

--- tests/URLPrefixSenderTest.php
+++ tests/URLPrefixSenderTest.php
@@ -13,15 +13,29 @@ use SmartyStreets\PhpSdk\Tests\Mocks\MockSender;
 use PHPUnit\Framework\TestCase;
 
 class URLPrefixSenderTest extends TestCase {
-    public function testProvidedURLOverridesRequestURL() {
+    public function testProvidedURLPresent() {
         $request = new Request();
-        $request->setUrlPrefix("original");
-        $override = "override?";
+        $fake_url_prefix = "/fake_address_id";
+        $request->setUrlPrefix($fake_url_prefix); 
+        $original_url_prefix = "http://mysite.com/lookup";
+
         $inner = new MockSender(new Response(123, null, ""));
-        $sender = new URLPrefixSender($override, $inner);
+        $sender = new URLPrefixSender($original_url_prefix, $inner);
 
         $sender->send($request);
 
-        $this->assertEquals($override, $request->getUrl());
+        $this->assertEquals("http://mysite.com/lookup/fake_address_id?",$request->getUrl());
+    }
+
+    public function testProvidedURLNotPresent() {
+        $request = new Request();
+        $original_url_prefix = "http://mysite.com/lookup";
+
+        $inner = new MockSender(new Response(123, null, ""));
+        $sender = new URLPrefixSender($original_url_prefix, $inner);
+
+        $sender->send($request);
+
+        $this->assertEquals("http://mysite.com/lookup?", $request->getUrl());
     }
 }
