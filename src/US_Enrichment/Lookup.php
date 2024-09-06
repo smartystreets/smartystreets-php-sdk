f9c5f6b6a99f4036d84301539ef1269530cf1df7
Eric Devenport <eric@Eric-Devenports-MacBook-Air.local>
2024-09-06 16:14:19 -0600

    Added Address Search Feature for the US Address Enrichment API

--- src/US_Enrichment/Lookup.php
+++ src/US_Enrichment/Lookup.php
@@ -7,22 +7,52 @@ class Lookup {
     //region [ Fields ]
 
     private $smartyKey,
+        $freeform,
+        $street,
+        $city,
+        $state,
+        $zipcode,
         $dataSetName,
         $dataSubsetName,
         $response;
 
     //endregion
 
-    public function __construct($smartyKey, $dataSetName, $dataSubsetName) {
+    public function __construct($smartyKey = null, $dataSetName = null, $dataSubsetName = null, $freeform = null, $street = null, $city = null, $state = null,
+    $zipcode = null) {
         $this->smartyKey = $smartyKey;
         $this->dataSetName = $dataSetName;
         $this->dataSubsetName = $dataSubsetName;
+        $this->freeform = $freeform;
+        $this->street = $street;
+        $this->city = $city;
+        $this->state = $state;
+        $this->zipcode = $zipcode;
         $this->response = null;
-
     }
 
     public function getSmartyKey(){
         return $this->smartyKey;
+    }
+
+    public function getFreeform(){
+        return $this->freeform;
+    }
+    
+    public function getStreet(){
+        return $this->street;
+    }
+
+    public function getCity(){
+        return $this->city;
+    }
+
+    public function getState(){
+        return $this->state;
+    }
+
+    public function getZipcode(){
+        return $this->zipcode;
     }
 
     public function getDataSetName(){
@@ -35,6 +65,38 @@ class Lookup {
 
     public function getResponse() {
         return $this->response;
+    }
+
+    public function setSmartyKey($smartyKey) {
+        $this->smartyKey = $smartyKey;
+    }
+
+    public function setDataSetName($dataSetName) {
+        $this->dataSetName = $dataSetName;
+    }
+
+    public function setDataSubsetName($dataSubsetName) {
+        $this->dataSubsetName = $dataSubsetName;
+    }
+
+    public function setFreeform($freeform) {
+        $this->freeform = $freeform;
+    }
+
+    public function setStreet($street){
+        $this->street = $street;
+    }
+
+    public function setCity($city){
+        $this->city = $city;
+    }
+
+    public function setState($state){
+        $this->state = $state;
+    }
+
+    public function setZipcode($zipcode){
+        $this->zipcode = $zipcode;
     }
 
     public function setResponse($response){
